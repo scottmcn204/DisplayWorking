937b666bcfe1429fe726932fc0f157f9a190d912
scottmcn204 <77010334+scottmcn204@users.noreply.github.com>
2022-02-28 15:25:31 +0000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+const menuBtn = document.querySelector('.menuBtn')
+const sidebar = document.querySelector('.sidebar')
+let menuOpen = false;
+menuBtn.addEventListener('click', () => {
+    if(!menuOpen) {
+        menuBtn.classList.add('open');
+        //document.getElementById('sidebar').style.display = "flex";
+        menuOpen = true;
+    } else {
+        menuBtn.classList.remove('open');
+        //document.getElementById('sidebar').style.display = "none";
+        menuOpen = false;
+    }
+});
