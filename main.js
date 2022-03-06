8b15cd9e9011047c3511254524b123ac8ba5ba03
Scott McNally <scottamcnally@gmail.com>
2022-03-06 18:27:33 +0000

    gg

--- main.js
+++ main.js
@@ -5,24 +5,26 @@ menuBtn.addEventListener('click', () => {
         menuBtn.classList.add('open');
         menuOpen = true;
         document.querySelector('.sidebar').classList.add('open')
+        document.querySelector('.dim').classList.add('open')
     } else {
         menuBtn.classList.remove('open');
         menuOpen = false;
         document.querySelector('.sidebar').classList.remove('open')
+        document.querySelector('.dim').classList.remove('open')
     }
 });
 
-const loginForm = document.querySelector('login-form')
-const loginButton = document.querySelector('login-form-submit')
-loginButton.addEventListener('click', (e) => {
+const loginForm = document.querySelector('#login-form')
+const loginButton = document.querySelector('#login-form-submit')
+loginButton.addEventListener("click", (e) => {
     e.preventDefault();
     const username = loginForm.username.value;
     const password = loginForm.password.value;
-    if(username === 'admin' && password === 'admin'){
+    if (username === 'admin' && password === 'admin1'){
         alert("You have logged in!");
-        window.open('Data.html');
+        window.location.replace('Data.html');
     }
     else{
         alert("Incorrect Password");
     }
-});
+})
